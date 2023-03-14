9d5fe3bc513ef8ef2263f2644b3a0c20b35ad477
Aqeel <aqeelten@gmail.com>
2023-03-14 20:20:25 +0530

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -25,63 +25,98 @@ class _MyWidgetState extends State<MyWidget> {
         appBar: AppBar(
           title: const Text('Hello World'),
         ),
-        body: SafeArea(
-          child: Container(
-            decoration: const BoxDecoration(
-                color: Colors.white, shape: BoxShape.rectangle),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    '$_counter',
-                    style: const TextStyle(
-                      fontSize: 30,
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Text(
-                    'hello',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  const Icon(
-                    Icons.star,
-                    color: Colors.red,
-                    size: 50,
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.orange,
-                  ),
-                ],
-              ),
-            ),
-          ),
+        body: const SafeArea(
+          child: MyContainer(),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          setState(() {
-            _counter++;
-          });
-        }),
+        floatingActionButton: FloatingActionButton(
+            child: Container(
+                padding: const EdgeInsets.all(5),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: Text("$_counter")),
+            onPressed: () {
+              setState(() {
+                _counter++;
+              });
+            }),
       ),
+    );
+  }
+}
+
+class MyContainer extends StatefulWidget {
+  const MyContainer({
+    super.key,
+  });
+
+  @override
+  State<MyContainer> createState() => _MyContainerState();
+}
+
+class _MyContainerState extends State<MyContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration:
+          const BoxDecoration(color: Colors.white, shape: BoxShape.rectangle),
+      child: const MyColumn(),
+    );
+  }
+}
+
+class MyColumn extends StatefulWidget {
+  const MyColumn({
+    super.key,
+  });
+
+  @override
+  State<MyColumn> createState() => _MyColumnState();
+}
+
+class _MyColumnState extends State<MyColumn> {
+  List<int> _numbers = [];
+  final List<String> _names = [
+    "Flutter",
+    "Dart",
+    "Java",
+    "Kotlin",
+    "C++",
+    "C#",
+    "C",
+    "Python"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: _names.length,
+      padding: const EdgeInsets.all(20),
+      itemBuilder: (context, index) {
+        return Container(
+          height: 50,
+          margin: const EdgeInsets.only(bottom: 10),
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              const CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.person),
+              ),
+              Text(
+                _names[index],
+                style: const TextStyle(fontSize: 20),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
